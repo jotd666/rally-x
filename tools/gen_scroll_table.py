263803b15446b5e09c96bb4ce2df833f29672316
Jean-François Fabre <jotd666@gmail.com>
2025-08-16 15:12:29 +0200

    sprite priorities fixed

--- tools/gen_scroll_table.py
+++ tools/gen_scroll_table.py
@@ -9,7 +9,7 @@ src_dir = this_dir / os.pardir / "src" / "amiga"
 
 
 # table with fine shift + byte offset
-def doit(width):
+def doit(width,status_static_shift):
     asm_output = src_dir / f"scroll_table_{width}.68k"
     wmask = width-1
 
@@ -22,7 +22,7 @@ def doit(width):
         shiftval_msb = ((shift&(wmask & 0x30))>>2)      # 2 high bits H7 H6
         items.append( (shiftval_msb<<4)) # put same shift for both "playfields"
         shiftval_lsb = (shift&0xF)                      # 4 low bits H5->H2
-        items.append( (shiftval_lsb<<4)) # put same shift for both "playfields"
+        items.append( (status_static_shift | shiftval_lsb<<4))
 
         items.append(0)
         items.append(offset)
@@ -33,5 +33,5 @@ def doit(width):
     return items
 
 if __name__ == "__main__":
-    doit(width = 16)   # FMODE=1
+    doit(width = 16,status_static_shift = 3)   # FMODE=1
 
